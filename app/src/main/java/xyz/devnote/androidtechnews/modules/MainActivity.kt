929ee74dbaaac55fb2d87828666913499f68d39f
Weeravit Sirita <weeravit.sirita@gmail.com>
2017-11-12 23:34:20 +0700

    add main application

---
+++ app/src/main/java/xyz/devnote/androidtechnews/modules/MainActivity.kt
@@ -0,0 +1,13 @@
+package xyz.devnote.androidtechnews.modules
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import xyz.devnote.androidtechnews.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
